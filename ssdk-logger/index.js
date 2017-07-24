7eea2d7dcfd7467689a5a9b2b7c5e0742094ce20
Phoenix Song <C.Song@alumni.ubc.ca>
2017-07-23 21:16:22 -0700

    1.0 proposal

--- ssdk-logger/index.js
+++ ssdk-logger/index.js
@@ -4,27 +4,32 @@
  * @author Phoenix(github.com/azusa0127)
  * @version 1.0.0
  */
+/**
+ * ============================================================================
+ * Requires.
+ * ============================================================================
+ */
 const path = require(`path`);
 const { createWriteStream } = require(`fs`);
-
+/**
+ * ============================================================================
+ * Library.
+ * ============================================================================
+ */
 class Logger {
   /**
    * Logger Constructor
-   *
-   * @param {object} Options the only acceptable argument.
    *
    * @param {string|number} [Options.level=`info`] Log Level. Determines the verboseness of the logger.
    * @param {string} [Options.prefix=``] Prefix label. The Label showed infront of every message.
    * @param {number} [Options.indent=0] Initial indentation. The initial number of spaces to indent.
    * @param {stream.Writable|stream.Writable[]} [Options.outStream=console] The output stream(s) for the console, can be either a single stream for both stdout and stderr or [stdout, stderr].
    */
-  constructor({ level, prefix, indent, outStream } = {}) {
+  constructor({ level = `info`, prefix = ``, indent = 0, outStream } = {}) {
     this.LEVELS = [`error`, `warn`, `info`, `log`, `debug`, `trace`];
-    this.logLevel = level
-      ? typeof level === `string` ? this.LEVELS.indexOf(level) : level
-      : this.LEVELS.indexOf(`info`);
-    this.logPrefix = prefix && typeof prefix === `string` ? `[${prefix}]` : ``;
-    this.logIndent = indent && typeof indent === `number` ? indent : 0;
+    this.logLevel = typeof level === `string` ? this.LEVELS.indexOf(level) : level;
+    this.logPrefix = typeof prefix === `string` ? prefix : ``;
+    this.logIndent = typeof indent === `number` ? indent : 0;
     this.Console = outStream
       ? Array.isArray(outStream)
         ? new console.Console(...outStream)
@@ -36,35 +41,37 @@ class Logger {
    * Internal write function, controls output template and output level control.
    *
    * @param {any} data Data to be written.
-   * @param {string|number} [channel=`info`] Log Channel. Can be level string or its array index.
-   * @param {string|Buffer} [prefix=null] Prefix label. uses logPrefix if null.
+   * @param {string|number} [Options.channel=`info`] Log Channel. Can be level string or its array index.
+   * @param {string|Buffer} [Options.prefix=``] Sub-Prefix label.
+   * @param {bool} [rawFormat=false] If output in the raw format.
    */
-  _write(data, channel = `info`, prefix = null) {
-    if ((typeof channel === `string` ? this.LEVELS.indexOf(channel) : channel) <= this.logLevel) {
+  _write(data, { channel = `info`, prefix = ``, rawFormat = false }) {
+    if (typeof channel === `number`) channel = this.LEVELS[channel];
+    if (this.LEVELS.indexOf(channel) <= this.logLevel) {
       // Debug output optimization
-      if (
-        typeof data !== `string` &&
-        (channel === `debug` || channel === this.LEVELS.indexOf(`debug`))
-      )
-        data = JSON.stringify(data, null, 2);
-
-      const timeStamp = new Date().toLocaleString();
-      if (prefix && prefix.length) prefix = `<${prefix}>`;
-      const template = `${timeStamp} |${channel.toUpperCase()}| ${prefix
-        ? `${` `.repeat(this.logIndent + this.logPrefix.length)} ${prefix}`
-        : `${this.logPrefix}${` `.repeat(this.logIndent)}`} ${data}`;
+      if (channel === `debug` && typeof data !== `string`) data = JSON.stringify(data, null, 2);
+      const timeStamp = new Date().toLocaleString(),
+        prefixes = `${timeStamp}|${channel.toUpperCase()}${` `.repeat(5 - channel.length)}${this
+          .logPrefix.length
+          ? `|${this.logPrefix}`
+          : ``}|${` `.repeat(this.logIndent)}${prefix.length ? `<${prefix}> ` : ``}`,
+        message = rawFormat
+          ? data
+          : `${prefixes}${data && typeof data === `string`
+              ? data.replace(/\n/g, `\n${` `.repeat(prefixes.length)}`)
+              : data}`;
       switch (channel) {
         case `error`:
         case `warn`:
-          this.Console.error(template);
+          this.Console.error(message);
           break;
         case `info`:
         case `log`:
         case `debug`:
-          this.Console.log(template);
+          this.Console.log(message);
           break;
         case `trace`:
-          this.Console.trace(template);
+          this.Console.trace(message);
           break;
         default:
           throw new Error(`Invalid Channel ${channel}!`);
@@ -78,7 +85,7 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   error(data, prefix = ``) {
-    this._write(data, `error`, prefix);
+    this._write(data, { channel:`error`, prefix});
   }
   /**
    * Log data on `warn` channel with Console.error().
@@ -87,7 +94,7 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   warn(data, prefix = ``) {
-    this._write(data, `warn`, prefix);
+    this._write(data, { channel:`warn`, prefix});
   }
   /**
    * Log data on `info` channel with Console.log().
@@ -96,7 +103,7 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   info(data, prefix = ``) {
-    this._write(data, `info`, prefix);
+    this._write(data, { channel:`info`, prefix});
   }
   /**
    * Log data on `log` channel with Console.log().
@@ -105,7 +112,7 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   log(data, prefix = ``) {
-    this._write(data, `log`, prefix);
+    this._write(data, { channel:`log`, prefix});
   }
   /**
    * Log data on `debug` channel with Console.log().
@@ -114,7 +121,7 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   debug(data, prefix = ``) {
-    this._write(data, `debug`, prefix);
+    this._write(data, { channel:`debug`, prefix});
   }
   /**
    * Log data on `trace` channel with Console.trace().
@@ -123,11 +130,19 @@ class Logger {
    * @param {string|Buffer} [prefix=``] Prefix label.
    */
   trace(data, prefix = null) {
-    this._write(data, `trace`, prefix);
+    this._write(data, { channel:`trace`, prefix});
   }
-
   /**
-   * Group the following logger calls by block and increase indentation.
+   * Output data in raw format.
+   *
+   * @param {any} data Data to be written.
+   * @param {string|number} [channel=`info`] Output Channel.
+   */
+  raw(data, channel = `info`) {
+    this._write(data, {channel, rawFormat:true});
+  }
+  /**
+   * Group the logger calls by block and indent following logs.
    *
    * @param {string|Buffer} [label=``] Group label.
    * @param {string|number} [channel=`info`] Log Channel. Can be level string or its array index.
@@ -137,7 +152,7 @@ class Logger {
     this.logIndent += 2;
   }
   /**
-   * Ending the logger call block and decrease indentation.
+   * Ending the logger call block and unindent logs after.
    *
    * @param {string|Buffer} [label=``] Group label.
    * @param {string|number} [channel=`info`] Log Channel. Can be level string or its array index.
@@ -149,10 +164,25 @@ class Logger {
 }
 
 class FileLogger extends Logger {
+  /**
+   * FileLogger Constructor
+   *
+   * @param {string|string[]} filename The name of the log files, can be either a single file for both stdout and stderr or a pair of files as [stdout, stderr].
+   *
+   * @param {string|number} [Options.level=`info`] Log Level. Determines the verboseness of the logger.
+   * @param {string} [Options.prefix=``] Prefix label. The Label showed infront of every message.
+   * @param {number} [Options.indent=0] Initial indentation. The initial number of spaces to indent.
+   */
   constructor(filename, { level, prefix, indent } = {}) {
-    const outStream = Array.isArray(filename) ? filename.map(x => path.resolve(x)).map(x => createWriteStream(x)) : createWriteStream(path.resolve(filename));
-    super({ level, prefix, indent, outStream});
+    const outStream = Array.isArray(filename)
+      ? filename.map(x => path.resolve(x)).map(x => createWriteStream(x))
+      : createWriteStream(path.resolve(filename));
+    super({ level, prefix, indent, outStream });
   }
 }
-
+/**
+ * ============================================================================
+ * Exports.
+ * ============================================================================
+ */
 module.exports = { Logger, FileLogger };
