847837f059a95051c1f9c959082928f73fcfad5a
Phoenix.S <PhoenixSong@Outlook.com>
2017-08-25 14:24:35 -0700

    suspension of types as for now.

--- ssdk-logger/index.d.ts
+++ ssdk-logger/index.d.ts
@@ -1,35 +1,20 @@
-declare namespace logger {
-  export type Channel = "error" | "warn" | "info" | "log" | "debug" | "trace";
-  export interface Logger {
-    LEVELS: Channel[];
-    logLevel: Channel;
-    logPrefix: string;
-    logIndent: number;
-    _console: Console;
-    /**
-   * Creates an instance of Logger.
-   * @param {object} [Options = { level = `info`, prefix = ``, indent = 0, outStream }]
-   * @param {string} [Options.level=`info`] Log Level. Determines the verboseness of the logger.
-   * @param {string} [Options.prefix=``] Prefix label. The Label showed infront of every message.
-   * @param {number} [Options.indent=0] Initial indentation. The initial number of spaces to indent.
-   * @param {stream.Writable|stream.Writable[]} [Options.outStream=[stdout, stderr]]
-   * @memberof Logger
-   */
-    constructor(options?: {
-      level?: string = `info`;
-      prefix?: string | Buffer = ``;
-      indent?: number = 0;
-      outStream?: WritableStream | WritableStream[];
-    });
-    /**
+import { Writable } from "stream";
+export type Channel = "error" | "warn" | "info" | "log" | "debug" | "trace";
+interface IBaseLogger {
+  LEVELS: Channel[];
+  logLevel: Channel;
+  logPrefix: string;
+  logIndent: number;
+  _console: Console;
+  /**
    * Internal channel input validator.
    *
    * @param {any} channel a channel/level input.
    * @returns {string} a valid channel string.
    * @memberof Logger
    */
-    _validateChannelInput(channel: Channel): string;
-    /**
+  _validateChannelInput(channel: Channel): string;
+  /**
    * Internal write function, controls output template and output level control.
    *
    * @param {any[]} data Data to be written.
@@ -39,16 +24,16 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    _write(
-      data: any,
-      option: {
-        channel?: Channel = `info`;
-        prefix?: string | Buffer = ``;
-        rawFormat?: boolean = false;
-        indentAfter?: number = 0;
-      },
-    ): boolean;
-    /**
+  _write(
+    data: any,
+    option: {
+      channel?: Channel;
+      prefix?: string | Buffer;
+      rawFormat?: boolean;
+      indentAfter?: number;
+    },
+  ): boolean;
+  /**
    * Log data on `error` channel with Console.error().
    *
    * @param {any} data Data to be written.
@@ -56,8 +41,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    error(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  error(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Log data on `warn` channel with Console.error().
    *
    * @param {any} data Data to be written.
@@ -65,8 +50,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    warn(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  warn(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Log data on `info` channel with Console.log().
    *
    * @param {any} data Data to be written.
@@ -74,8 +59,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    info(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  info(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Log data on `log` channel with Console.log().
    *
    * @param {any} data Data to be written.
@@ -83,8 +68,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    log(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  log(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Log data on `debug` channel with Console.log().
    *
    * @param {any} data Data to be written.
@@ -92,8 +77,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    debug(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  debug(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Log data on `trace` channel with Console.trace().
    *
    * @param {any} data Data to be written.
@@ -101,8 +86,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    trace(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  trace(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Output data without formating.
    *
    * @param {any} data Data to be written.
@@ -110,8 +95,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    raw(data: any, prefix?: string | Buffer = ``): boolean;
-    /**
+  raw(data: any, prefix?: string | Buffer): boolean;
+  /**
    * Group the logger calls by block and indent following logs.
    *
    * @param {string|Buffer} label Group label.
@@ -119,8 +104,8 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    enterBlock(label: string | Buffer, channel?: string = `info`): boolean;
-    /**
+  enterBlock(label: string | Buffer, channel?: string): boolean;
+  /**
    * Ending the logger call block and unindent logs after.
    *
    * @param {string|Buffer} label Group label.
@@ -128,19 +113,36 @@ declare namespace logger {
    * @return {boolean} If message emitted or not.
    * @memberof Logger
    */
-    exitBlock(label: string | Buffer, channel?: string = `info`): boolean;
-    /**
+  exitBlock(label: string | Buffer, channel?: string): boolean;
+  /**
    * Change the Instance Log Level.
    *
    * @param {string} [level=`info`]
    * @return {string} Previous Log Level pre-reassignment.
    * @memberof Logger
    */
-    setLogLevel(level?: string = `info`): string;
-  }
+  setLogLevel(level?: string): string;
+}
+export interface Logger extends IBaseLogger {
+  /**
+   * Creates an instance of Logger.
+   * @param {object} [Options = { level = `info`, prefix = ``, indent = 0, outStream }]
+   * @param {string} [Options.level=`info`] Log Level. Determines the verboseness of the logger.
+   * @param {string} [Options.prefix=``] Prefix label. The Label showed infront of every message.
+   * @param {number} [Options.indent=0] Initial indentation. The initial number of spaces to indent.
+   * @param {stream.Writable|stream.Writable[]} [Options.outStream=[stdout, stderr]]
+   * @memberof Logger
+   */
+  constructor(options?: {
+    level?: string;
+    prefix?: string | Buffer;
+    indent?: number;
+    outStream?: Writable | Writable[];
+  }): void;
+}
 
-  export interface FileLogger extends Logger {
-    /**
+export interface FileLogger extends IBaseLogger {
+  /**
    * FileLogger Constructor
    *
    * @param {string|string[]} filename The name of the log files, can be either a single file for both stdout and stderr or a pair of files as [stdout, stderr].
@@ -149,14 +151,8 @@ declare namespace logger {
    * @param {string} [Options.prefix=``] Prefix label. The Label showed infront of every message.
    * @param {number} [Options.indent=0] Initial indentation. The initial number of spaces to indent.
    */
-    constructor(
-      filename: string | string[],
-      options?: {
-        level?: string = `info`;
-        prefix?: string | Buffer = ``;
-        indent?: number = 0;
-      } = {},
-    );
-  }
+  constructor(
+    filename: string | string[],
+    options?: { level?: string; prefix?: string | Buffer; indent?: number },
+  ): void;
 }
-export = logger;
